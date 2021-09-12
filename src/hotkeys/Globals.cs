fcd03edd6972db8cd5c46b77627d6348b5f551db
apocc <46197112+apocc@users.noreply.github.com>
2021-09-12 15:12:19 +0200

    updated mod name

--- src/hotkeys/Globals.cs
+++ src/hotkeys/Globals.cs
@@ -6,7 +6,7 @@ namespace Apocc.Pw.Hotkeys
     public static class Globals
     {
         public static string LogPrefix = "[ApoccHotkeys] ";
-        public static string ModTitle = "Hotkeys for Weapon Sets, AI and Stealth";
+        public static string ModTitle = "Additional Hotkeys";
         public static string Sepatator = "---------------";
         public static void LogException(Exception e) => 
             Log.Error($"{Sepatator}{ModTitle}\n{e.Message}:\n{e.StackTrace}\n{Sepatator}", LogPrefix);
