fca07b3782d08e15e2bee69de92d8a28e96ca33f
apocc <46197112+apocc@users.noreply.github.com>
2021-09-16 20:34:48 +0200

    added gui char select definition

---
+++ src/hotkeys/Data/CharacterSelect/GuiCharacterSelect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Apocc.Pw.Hotkeys.Data.CharacterSelect
+{
+    internal sealed class GuiCharacterSelect
+    {
+        internal static readonly List<SettingsOption> Options = new List<SettingsOption>
+        {
+            new SettingsOption("<b>Enable character selection:</b>", "EnableCharSel", SettingsOptionType.CheckBox),
+            new SettingsOption("Key to select next character:", "CsNext", SettingsOptionType.Text, "__apocc__cs__next"),
+            new SettingsOption("Key to select previous character:", "CsPrev", SettingsOptionType.Text, "__apocc__cs__prev"),
+        };
+    }
+}
