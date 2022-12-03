a141c0861936f9a051223f1a2151f655864b12c9
apocc <46197112+apocc@users.noreply.github.com>
2022-12-03 16:31:16 +0100

    updated assembly info

--- src/hotkeys/Properties/AssemblyInfo.cs
+++ src/hotkeys/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("HotKeys")]
 [assembly: AssemblyProduct("HotKeys")]
-[assembly: AssemblyCopyright("Copyright ©apocc  2021")]
+[assembly: AssemblyCopyright("Copyright ©apocc 2021-2022")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
